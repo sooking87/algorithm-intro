215e95b770b921924bdc4373beb682090d9305fb
sooking87 <sksohn01@naver.com>
2023-04-16 20:53:07 +0900

    temp

--- temp.py
+++ temp.py
@@ -1,26 +1,30 @@
-import queue
+def powerMat(x, n):
+    if n == 1:
+        return x
+    elif n % 2 == 0:
+        return powerMat(multMat(x, x), n // 2)
+    else:
+        return multMat(x, powerMat(multMat(x, x), (n - 1) // 2))
 
 
-def bfs(graph, start):
-    visited = {start}
-    que = queue.Queue()
-    que.put(start)
-    while not que.empty():
-        v = que.get()
-        print(v, end=' ')
-        nbr = graph[v] - visited
-        for u in nbr:
-            visited.add(u)
-            que.put(u)
+# 행렬을 곱하는 multMat(M1, M2) 함수에요!
+def multMat(M1, M2):
+    result = [[0 for _ in range(len(M2[0]))] for __ in range(len(M1))]
+    for i in range(len(M1)):
+        for j in range(len(M2[0])):
+            for k in range(len(M1[0])):
+                result[i][j] += M1[i][k] * M2[k][j]
+    return result
 
 
 n = int(input())
-mygraph = dict()
-for i in range(n):
-    a, b = input().split()
-    mygraph[a] = mygraph.setdefault(a, set()) | {b}
-    mygraph[b] = mygraph.setdefault(b, set()) | {a}
+n2 = int(input())
+x = [[int(i) for i in input().split()] for _ in range(n2)]
 
-print('BFS : ', end=' ')
-bfs(mygraph, "A")
+# 출력합니다!
+result = powerMat(x, n)
 print()
+for row in result:
+    for num in row:
+        print(num, end=' ')
+    print()
